b29a29a7da56fb3428dfd93f02411fd7c06d88ba
lushiykoti1k <116806089+lushiykoti1k@users.noreply.github.com>
2023-12-26 15:08:38 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# lushay-laba-2
+# mobile-swift
+Приложение на swift, выполнила Долинина Анастасия, МПБ 101-0-01
+
+![firstScreen](Assets/Screenshot_1.png)
+
+![secondScreen](Assets/Screenshot_2.png)
---
+++ SwiftUI Lab/SwiftUI lab/SwiftUI lab/SwiftUI_labApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_labApp.swift
+//  SwiftUI lab
+//
+//  Created by Nikita on 01.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_labApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
